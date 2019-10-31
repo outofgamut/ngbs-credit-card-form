746c2035bd849e67cd4f72a1520bd5dc5ab59570
John Salvie <john.salvie@associa.us>
2019-10-30 22:23:23 -0500

    clean up storybook config files to align with nx

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,11 @@
 import { configure } from '@storybook/angular';
 
 // automatically import all files ending in *.stories.ts
-// const req = require.context('../src/stories', true, /\.stories\.ts$/);
-// const appReq = require.context('../projects', true, /\.stories\.ts$/);
-const appReq = require.context('../libs', true, /\.stories\.ts$/);
+// configure(require.context('../libs', true, /\.stories\.ts$/), module);
+
+const req = require.context('../libs', true, /\.stories\.ts$/);
 function loadStories() {
-  // req.keys().forEach(filename => req(filename));
-  appReq.keys().forEach(filename => appReq(filename));
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);

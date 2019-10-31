9f1b02cf345295050109f0f45e4a496be42a81e5
John Salvie <john.salvie@associa.us>
2019-10-30 21:58:30 -0500

    fix storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,11 @@
 import { configure } from '@storybook/angular';
 
 // automatically import all files ending in *.stories.ts
-const req = require.context('../src/stories', true, /\.stories\.ts$/);
-const appReq = require.context('../projects', true, /\.stories\.ts$/);
+// const req = require.context('../src/stories', true, /\.stories\.ts$/);
+// const appReq = require.context('../projects', true, /\.stories\.ts$/);
 const appReq = require.context('../libs', true, /\.stories\.ts$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  // req.keys().forEach(filename => req(filename));
   appReq.keys().forEach(filename => appReq(filename));
 }
 
